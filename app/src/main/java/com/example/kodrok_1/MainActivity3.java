40354de5be0e9d204cdbe498e6b4318c294c50f5
yukky9 <kozhurinayulia@gmail.com>
2024-04-09 12:22:25 +0300

    phone sign in

--- app/src/main/java/com/example/kodrok_1/MainActivity3.java
+++ app/src/main/java/com/example/kodrok_1/MainActivity3.java
@@ -2,7 +2,9 @@ package com.example.kodrok_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+
+    public void Click(View view) {
+        Intent intent = new Intent(this, MainActivity4.class);
+        startActivity(intent);
     }
 }
