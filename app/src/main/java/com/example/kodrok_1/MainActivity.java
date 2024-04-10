651cdcbc7afdd61aab229c47d68afad0afa63075
yukky9 <kozhurinayulia@gmail.com>
2024-04-10 12:42:56 +0300

    all

--- app/src/main/java/com/example/kodrok_1/MainActivity.java
+++ app/src/main/java/com/example/kodrok_1/MainActivity.java
@@ -2,20 +2,30 @@ package com.example.kodrok_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tw;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void Click(View view) {
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
+        tw = findViewById(R.id.textView20);
+
+        tw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kodrok_1/MainActivity2.java
+++ app/src/main/java/com/example/kodrok_1/MainActivity2.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    Button bt1;
+    ImageButton bt1;
+    ImageButton bt2;
     TextView textView;
 
     @SuppressLint("WrongViewCast")
@@ -22,10 +25,29 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         bt1 = findViewById(R.id.imageButton4);
-        textView = findViewById(R.id.textView6);
-    }
+        bt2 = findViewById(R.id.imageButton2);
+        textView = findViewById(R.id.textView10);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bt1.setImageResource(R.drawable.rectangle_44);
+                bt2.setImageResource(R.drawable.rectangle_4);
 
-    public void Click(View view) {
-        bt1.setBackgroundColor(Color.BLUE);
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bt1.setImageResource(R.drawable.rectangle_4);
+                bt2.setImageResource(R.drawable.rectangle_44);
+            }
+        });
     }
 }
