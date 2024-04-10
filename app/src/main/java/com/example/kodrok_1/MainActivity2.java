8758b220a1ea4adcc7a8d2a6d7af736d5a4498de
yukky9 <kozhurinayulia@gmail.com>
2024-04-10 10:49:20 +0300

    all

--- app/src/main/java/com/example/kodrok_1/MainActivity2.java
+++ app/src/main/java/com/example/kodrok_1/MainActivity2.java
@@ -2,25 +2,30 @@ package com.example.kodrok_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button bt1;
+    TextView textView;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        bt1 = findViewById(R.id.imageButton4);
+        textView = findViewById(R.id.textView6);
     }
 
     public void Click(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void Click1(View view) {
-        Intent intent = new Intent(this, MainActivity3.class);
-        startActivity(intent);
+        bt1.setBackgroundColor(Color.BLUE);
     }
 }
