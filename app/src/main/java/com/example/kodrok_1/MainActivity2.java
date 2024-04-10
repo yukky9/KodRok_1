4b4ed583d4c89ae8d835186de361fef7853d9f73
yukky9 <kozhurinayulia@gmail.com>
2024-04-10 13:20:28 +0300

    register

--- app/src/main/java/com/example/kodrok_1/MainActivity2.java
+++ app/src/main/java/com/example/kodrok_1/MainActivity2.java
@@ -9,16 +9,19 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    EditText email_admin;
+    EditText pswrd_admin;
     ImageButton bt1;
     ImageButton bt2;
     TextView textView;
 
-    @SuppressLint("WrongViewCast")
+    @SuppressLint({"WrongViewCast", "MissingInflatedId"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,8 @@ public class MainActivity2 extends AppCompatActivity {
         bt1 = findViewById(R.id.imageButton4);
         bt2 = findViewById(R.id.imageButton2);
         textView = findViewById(R.id.textView10);
+        email_admin = findViewById(R.id.editTextTextEmailAddress2);
+        pswrd_admin = findViewById(R.id.editTextNumberPassword);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,7 +44,8 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View view) {
                 bt1.setImageResource(R.drawable.rectangle_44);
                 bt2.setImageResource(R.drawable.rectangle_4);
-
+                email_admin.setHint("Enter the supervisor's email");
+                pswrd_admin.setHint("");
             }
         });
         bt2.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +53,8 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View view) {
                 bt1.setImageResource(R.drawable.rectangle_4);
                 bt2.setImageResource(R.drawable.rectangle_44);
+                pswrd_admin.setHint("Enter code");
+                email_admin.setHint("");
             }
         });
     }
